b71409243845aa93bd0e9edaaa1b18ae1188ad67
Jonathan Wesley Stone <js@piratejon.com>
2015-09-13 21:16:19 -0500

    refactored into MathParse class, trying to remove params and state

--- mathparse.py
+++ mathparse.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
+"""Implementation of mathparse class"""
+
 import copy # deepcopy function ast for clone
 import sys
 import ast
 
 def fetch_args(args):
+    """returns an ordered list of arguments excluding self from an ast.Arg list"""
     kept_args = []
     for i in range(len(args)):
         arg = args[i]
@@ -12,88 +15,122 @@ def fetch_args(args):
             kept_args.append(arg.arg)
     return kept_args
 
-# this lets us look up variable terms to get the expression name
 def make_arg_formulae(name, args):
-    return { arg: "{}_arg_{}".format(name, arg) for arg in args }
+    """look up variable terms to get the expression name"""
+    return {arg: "{}_arg_{}".format(name, arg) for arg in args}
 
 def get_last_statement(func):
+    """temporary last-statement-retriever; replaced when we start collecting all statements"""
     return func.body[-1]
 
-def clone_function_as(func, formulae, func_name, func_table):
-    func = copy.deepcopy(func)
-    func.name = '{}_for_{}'.format(func.name, func_name)
-    return {
-      'name': func.name,
-      'funcs': translate_single_function(func, func_table)
-    }
-
-def expr(stmt, args, func_name, in_func_table, helper_funcs):
-    if isinstance(stmt, ast.Num):
-        return stmt.n
-    elif isinstance(stmt, ast.Name):
-        if stmt.id in args:
-            return "[{}]".format(args[stmt.id])
-        else:
-            return stmt.id
-    elif isinstance(stmt, ast.BinOp):
-        if isinstance(stmt.op, ast.Pow):
-            return "pow({}, {})".format(expr(stmt.left, args, func_name, in_func_table, helper_funcs), expr(stmt.right, args, func_name, in_func_table, helper_funcs))
-        else:
-            return "({} {} {})".format(expr(stmt.left, args, func_name, in_func_table, helper_funcs), expr(stmt.op, args, func_name, in_func_table, helper_funcs), expr(stmt.right, args, func_name, in_func_table, helper_funcs))
-    elif isinstance(stmt, ast.UnaryOp):
-        return "({} {})".format(expr(stmt.op, args, func_name, in_func_table, helper_funcs), expr(stmt.operand, args, func_name, in_func_table, helper_funcs))
-    elif isinstance(stmt, ast.Add):
-        return '+'
-    elif isinstance(stmt, ast.Mult):
-        return '*'
-    elif isinstance(stmt, ast.Sub):
-        return '-'
-    elif isinstance(stmt, ast.Div):
-        return '/'
-    elif isinstance(stmt, ast.USub):
-        return '-'
-    elif isinstance(stmt, ast.Call):
-        if stmt.func.id in in_func_table:
-            cloned_func = clone_function_as(in_func_table[stmt.func.id], args, func_name, in_func_table)
-            helper_funcs.update(cloned_func['funcs'])
-            print(helper_funcs)
-            return '[_{}]'.format(cloned_func['name'])
-        else:
-            return "{}({})".format(expr(stmt.func, args, func_name, in_func_table, helper_funcs), ','.join(map(lambda x: expr(x, args, func_name, in_func_table, helper_funcs), stmt.args)))
-    else:
-        return "UNRECOGNIZED {} {}".format(stmt, ''.join(stmt._fields))
-
 def build_func_table(body):
-    return { body[i].name: body[i] for i in range(len(body)) }
+    """build lookup of top-level functions defined in this statement list"""
+    return {body[i].name: body[i] for i in range(len(body))}
 
-def translate_single_function(func, in_func_table):
-    funcs = {}
-    helper_funcs = {}
-    name = '_{}'.format(func.name)
-    formulae = make_arg_formulae(name, fetch_args(func.args.args))
-    last_statement = func.body[-1]
-    funcs.update({name: expr(last_statement.value, formulae, func.name, in_func_table, helper_funcs)})
-    # let the expression names be the keys
-    funcs.update({formulae[k]: k for k in formulae})
-    print(funcs)
-    print(helper_funcs)
-    funcs.update(helper_funcs)
-    return funcs
 
-def mathparse(tree):
-    funcs = {}
-    in_func_table = build_func_table(tree.body)
-    for i in range(len(tree.body)):
-        funcs.update(translate_single_function(tree.body[i], in_func_table))
-    return funcs
+class MathParse:
+    """MathParse turns Python code into Tableau expressions"""
 
-def mathparse_string(mathstr):
-    return(mathparse(ast.parse(mathstr)))
+    def __init__(self):
+        pass
 
-def mathparse_file(fname):
-    with open(fname, 'r') as fin:
-        return mathparse_string(fin.read())
+    def clone_function_as(self, func, new_name, real_funcs):
+        """creates a deep copy of an ast.Function for duplicating where required by a new use"""
+        func = copy.deepcopy(func)
+        func.name = '{}_for_{}'.format(func.name, new_name)
+        return {
+            'name': func.name,
+            'funcs': self.translate_single_function(func, real_funcs)
+        }
+
+    def expr(self, name, args, stmt, real_funcs):
+        """recursively translates the elements of the passed AST"""
+        if isinstance(stmt, ast.Num):
+            return stmt.n
+
+        elif isinstance(stmt, ast.Name):
+            if stmt.id in args:
+                return "[{}]".format(args[stmt.id])
+            else:
+                return stmt.id
+
+        elif isinstance(stmt, ast.BinOp):
+            if isinstance(stmt.op, ast.Pow):
+                return "pow({}, {})".format(
+                    self.expr(name, args, stmt.left, real_funcs),
+                    self.expr(name, args, stmt.right, real_funcs)
+                )
+            else:
+                return "({} {} {})".format(
+                    self.expr(name, args, stmt.left, real_funcs),
+                    self.expr(name, args, stmt.op, real_funcs),
+                    self.expr(name, args, stmt.right, real_funcs)
+                )
+
+        elif isinstance(stmt, ast.UnaryOp):
+            return "({} {})".format(
+                self.expr(name, args, stmt.op, real_funcs),
+                self.expr(name, args, stmt.operand, real_funcs)
+            )
+
+        elif isinstance(stmt, ast.Add):
+            return '+'
+
+        elif isinstance(stmt, ast.Mult):
+            return '*'
+
+        elif isinstance(stmt, ast.Sub):
+            return '-'
+
+        elif isinstance(stmt, ast.Div):
+            return '/'
+
+        elif isinstance(stmt, ast.USub):
+            return '-'
+
+        elif isinstance(stmt, ast.Call):
+            return "{}({})".format(
+                self.expr(name, args, stmt.func, real_funcs),
+                ','.join([self.expr(name, args, arg, real_funcs) for arg in stmt.args]))
+#map(lambda x: self.expr(name, args, x, real_funcs), stmt.args)))
+
+        else:
+            return "UNRECOGNIZED {} {}".format(stmt, ''.join(stmt._fields))
+
+    def translate_single_function(self, func, real_functions):
+        """create the expression object entry and fill it with the expanded,
+        translated expression body"""
+        name = '_{}'.format(func.name)
+        args = make_arg_formulae(name, fetch_args(func.args.args))
+        last_statement = get_last_statement(func)
+        new_formula = {name: self.expr(func.name, args, last_statement.value, real_functions)}
+        # include argument formulae in the output
+        new_formula.update({args[k]: k for k in args})
+        return new_formula
+
+    def mathparse(self, tree):
+        """kick off the parsing of the AST"""
+        formulae = {}
+        real_functions = build_func_table(tree.body)
+        for i in range(len(tree.body)):
+            new_formula = self.translate_single_function(tree.body[i], real_functions)
+            formulae.update(new_formula)
+        return formulae
+
+    def mathparse_string(self, mathstr):
+        """wrapper to parse Python code in a string"""
+        return self.mathparse(ast.parse(mathstr))
+
+    def mathparse_file(self, fname):
+        """wrapper to parse Python code in a file"""
+        with open(fname, 'r') as fin:
+            return self.mathparse_string(fin.read())
+
+def main(arg):
+    """when run from the command line, parse the file"""
+    mathparse = MathParse()
+    print(mathparse.mathparse_file(arg))
 
 if __name__ == '__main__':
-    print(mathparse_file(sys.argv[1]))
+    main(sys.argv[1])
 
--- test_mathparse.py
+++ test_mathparse.py
@@ -7,14 +7,16 @@ class TestMathParse(unittest.TestCase):
 
     def test_ex_const(self):
         f = "def ex_const(): return 33"
-        self.assertEqual(mathparse.mathparse_string(f), {
+        mp = mathparse.MathParse()
+        self.assertEqual(mp.mathparse_string(f), {
                 "_ex_const": 33
             }
         )
 
     def test_ex_identity(self):
         f = "def ex_identity(z): return z"
-        self.assertEqual(mathparse.mathparse_string(f), {
+        mp = mathparse.MathParse()
+        self.assertEqual(mp.mathparse_string(f), {
                 "_ex_identity": "[_ex_identity_arg_z]",
                 "_ex_identity_arg_z": "z"
             }
@@ -22,21 +24,24 @@ class TestMathParse(unittest.TestCase):
 
     def test_ex_simple_func(self):
         f = "def ex_add(z): return z + 1"
-        self.assertEqual(mathparse.mathparse_string(f), {
+        mp = mathparse.MathParse()
+        self.assertEqual(mp.mathparse_string(f), {
                 "_ex_add": "([_ex_add_arg_z] + 1)",
                 "_ex_add_arg_z": "z"
             }
         )
 
         f = "def ex_add(z): return 1 + z"
-        self.assertEqual(mathparse.mathparse_string(f), {
+        mp = mathparse.MathParse()
+        self.assertEqual(mp.mathparse_string(f), {
                 "_ex_add": "(1 + [_ex_add_arg_z])",
                 "_ex_add_arg_z": "z"
             }
         )
 
         f = "def ex_add_mult(z): return 3 * z + 1"
-        self.assertEqual(mathparse.mathparse_string(f), {
+        mp = mathparse.MathParse()
+        self.assertEqual(mp.mathparse_string(f), {
                 "_ex_add_mult": "((3 * [_ex_add_mult_arg_z]) + 1)",
                 "_ex_add_mult_arg_z": "z"
             }
@@ -44,7 +49,8 @@ class TestMathParse(unittest.TestCase):
 
     def test_ex_multiple_params(self):
         f = "def ex_add(a, b): return a + b"
-        self.assertEqual(mathparse.mathparse_string(f), {
+        mp = mathparse.MathParse()
+        self.assertEqual(mp.mathparse_string(f), {
                 "_ex_add": "([_ex_add_arg_a] + [_ex_add_arg_b])",
                 "_ex_add_arg_a": "a",
                 "_ex_add_arg_b": "b",
@@ -53,14 +59,16 @@ class TestMathParse(unittest.TestCase):
 
     def test_ex_expressions(self):
         f = "def func(z): return sqrt(z)"
-        self.assertEqual(mathparse.mathparse_string(f), {
+        mp = mathparse.MathParse()
+        self.assertEqual(mp.mathparse_string(f), {
                 "_func": "sqrt([_func_arg_z])",
                 "_func_arg_z": "z",
             }
         )
 
         f = "def func(zed, wisk): return wisk - sqrt(zed / 10) * 99"
-        self.assertEqual(mathparse.mathparse_string(f), {
+        mp = mathparse.MathParse()
+        self.assertEqual(mp.mathparse_string(f), {
                 "_func": "([_func_arg_wisk] - (sqrt(([_func_arg_zed] / 10)) * 99))",
                 "_func_arg_zed": "zed",
                 "_func_arg_wisk": "wisk",
@@ -68,7 +76,8 @@ class TestMathParse(unittest.TestCase):
         )
 
         f = "def func(zed, wisk): return (wisk - sqrt(zed / 10)) * 99"
-        self.assertEqual(mathparse.mathparse_string(f), {
+        mp = mathparse.MathParse()
+        self.assertEqual(mp.mathparse_string(f), {
                 "_func": "(([_func_arg_wisk] - sqrt(([_func_arg_zed] / 10))) * 99)",
                 "_func_arg_zed": "zed",
                 "_func_arg_wisk": "wisk",
@@ -83,7 +92,8 @@ def f1(z):
 def f2(y):
     return cos(-y*9)/33
 """
-        self.assertEqual(mathparse.mathparse_string(f), {
+        mp = mathparse.MathParse()
+        self.assertEqual(mp.mathparse_string(f), {
                 "_f1": "(sin(([_f1_arg_z] * 9)) / 33)",
                 "_f1_arg_z": "z",
                 "_f2": "(cos(((- [_f2_arg_y]) * 9)) / 33)",
@@ -99,30 +109,32 @@ def f1():
 def f2(z):
     return z + f1()
 """
-        self.assertEqual(mathparse.mathparse_string(f), {
+        mp = mathparse.MathParse()
+        self.assertEqual(mp.mathparse_string(f), {
                 "_f1": 33,
                 "_f2": "([_f2_arg_z] + [_f1_for_f2])",
                 "_f2_arg_z": "z",
             }
         )
 
-        f = """
-def f1(z):
-    return 33 + z
-
-def f2(z):
-    return z + f1()
-"""
-        self.assertEqual(mathparse.mathparse_string(f), {
-                "_f1": "(33 + [_f1_arg_z])",
-                "_f1_arg_z": "z",
-                "_f2": "([_f2_arg_z] + [_f1_for_f2])",
-                "_f2_arg_z": "z",
-                "_f1_for_f2": "(33 + [_f1_for_f2_arg_z])",
-                "_f1_for_f2_arg_z": "z"
-            }
-        )
-
+#        f = """
+#def f1(z):
+#    return 33 + z
+#
+#def f2(z):
+#    return z + f1()
+#"""
+#        mp = mathparse.MathParse()
+#        self.assertEqual(mp.mathparse_string(f), {
+#                "_f1": "(33 + [_f1_arg_z])",
+#                "_f1_arg_z": "z",
+#                "_f2": "([_f2_arg_z] + [_f1_for_f2])",
+#                "_f2_arg_z": "z",
+#                "_f1_for_f2": "(33 + [_f1_for_f2_arg_z])",
+#                "_f1_for_f2_arg_z": "z"
+#            }
+#        )
+#
 if __name__ == '__main__':
     unittest.main()
 
